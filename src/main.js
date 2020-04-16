8ecb3f9c4e335b28cb0293cc0d302ea134e9e2a2
Elise Wetherholt <ewether@gmail.com>
2020-04-16 14:43:18 -0400

    added prettier

--- src/main.js
+++ src/main.js
@@ -1,58 +1,54 @@
 // grab user input from html
-const userInput = document.querySelector('.user-input');
+const userInput = document.querySelector(".user-input");
 // item list
-const itemList = document.querySelector('.item-list');
+const itemList = document.querySelector(".item-list");
 // list item
-const listItem = document.querySelector('.item-list li');
+const listItem = document.querySelector(".item-list li");
 // submit button
-const submitBtn = document.querySelector('.submit-btn');
-
+const submitBtn = document.querySelector(".submit-btn");
 
 // user input is added to new li
-submitBtn.addEventListener('click', function() {
-
+submitBtn.addEventListener("click", function () {
   // variable for creating brand new li node
-  const li = document.createElement('li');
-  li.classList.add('user-li');
+  const li = document.createElement("li");
+  li.classList.add("user-li");
 
   // finds length of userInput
   function liLength() {
     return userInput.value.length;
-  };
+  }
 
   // if userInput length is over 0, then this function can be called
   function addLi() {
     if (liLength() > 0) {
-    // the user's input that is stored in liContent is being appended to the new li node
-    li.appendChild(document.createTextNode(userInput.value));
-    // append the new li node (containing the user's input) to the item-list
-    itemList.appendChild(li);
-    // clear input box after new list item is added
-    userInput.value='';
-  }};
+      // the user's input that is stored in liContent is being appended to the new li node
+      li.appendChild(document.createTextNode(userInput.value));
+      // append the new li node (containing the user's input) to the item-list
+      itemList.appendChild(li);
+      // clear input box after new list item is added
+      userInput.value = "";
+    }
+  }
 
   // calls addLi function
   addLi();
 
-
-  li.addEventListener('click', function() {
-    li.classList.toggle('completed');
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
   });
 
-
   // delete button
-  const delBtn = document.createElement('button');
+  const delBtn = document.createElement("button");
   li.appendChild(delBtn);
-  delBtn.innerHTML = 'X';
+  delBtn.innerHTML = "X";
   // delBtn.classList.add('del-edit');
-  delBtn.setAttribute('id', 'delete-btn');
+  delBtn.setAttribute("id", "delete-btn");
 
   // when delBtn is clicked, li will be deleted
-    delBtn.addEventListener('click', function() {
-      // when clicked, add class 'delete'?
-      li.classList.add('delete');
-    });
-
+  delBtn.addEventListener("click", function () {
+    // when clicked, add class 'delete'?
+    li.classList.add("delete");
+  });
 
   // edit button
   // const editBtn = document.createElement('button');
@@ -70,27 +66,4 @@ submitBtn.addEventListener('click', function() {
   //     editBtn.contentEditable = 'false';
   //     delBtn.contentEditable = 'false';
   //   });
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
